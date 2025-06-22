3981870fa381bcc61425be0e72f970259f882c18
vilovnok <vilovnok@gmail.com>
2025-06-22 15:18:15 +0300

    base qwen

--- runv1.py
+++ runv1.py
@@ -8,19 +8,19 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 def run():        
     
     login(token="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")    
-    tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-1.5B", padding_side="right", truncation=True)
-    model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2.5-1.5B", device_map="auto", trust_remote_code=True)
-    model = PeftModel.from_pretrained(model, "../STAGE1-V1/checkpoint-4870/")
+    tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-1.5B", padding_side="right")
+    model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2.5-1.5B", device_map="auto")
+    # model = PeftModel.from_pretrained(model, "../STAGE1-V1/checkpoint-4870/")
 
-    merged_model = model.merge_and_unload()
-    save_path = "./STAGE1-V11/"
-    merged_model.save_pretrained(save_path)
+    # merged_model = model.merge_and_unload()
+    save_path = "./Base-Qwen/"
+    model.save_pretrained(save_path)
     tokenizer.save_pretrained(save_path)
 
     command = [
         "python", "-m", "vllm.entrypoints.openai.api_server",
-        "--model",  "./STAGE1-V11/",
-        "--tokenizer",  "./STAGE1-V11/",
+        "--model",  "./Base-Qwen/",
+        "--tokenizer",  "./Base-Qwen/",
         # "--gpu-memory-utilization", str(cfg.model.gpu_memory_utilization),         
         "--port", "7779",
         # "--dtype float16"
